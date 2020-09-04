d6987245dd1b491d00204627b15f1bb9dd8d3ba2
Metin Lamby <zsh git_setup.sh>
2020-09-04 18:37:49 +0200

    added the first landing draft

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -24,6 +24,7 @@ require("channels")
 
 // External imports
 import "bootstrap";
+import '../components/_navbar.js';
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
